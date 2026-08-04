d5e958175d265708e9103963ed61ec39593e251a
Furkan <ercelebifurkan@gmail.com>
2026-08-04 12:56:36 +0300

    .

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
-import { assets } from '../assets/assets'
+import React from "react";
+import { assets } from "../assets/assets";
 
-const StarRating = ({rating=4}) => {
+const StarRating = ({ rating = 4 }) => {
   return (
     <>
       {Array(5)
-          .fill(0)
-          .map((_, index) => (
-          <img src={rating > index ? assets.starIconFilled : assets.starIconOutlined} alt="star-icon"  className='w-4.5 h-4.5'/>
-          ))}
+        .fill(0)
+        .map((_, index) => (
+          <img
+            key={index}
+            src={
+              rating > index ? assets.starIconFilled : assets.starIconOutlined
+            }
+            alt="star-icon"
+            className="w-4.5 h-4.5"
+          />
+        ))}
     </>
-  )
-}
+  );
+};
 
-export default StarRating
+export default StarRating;
